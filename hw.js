4c7a7223b4871d42d7dc3783903c163312324edc
Tonle19 <145583832+Tonle19@users.noreply.github.com>
2023-10-11 19:05:45 +0700

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,61 @@
+// bai 1
+// let num = [1,2,3];
+// let sum = 0;
+// for (let i = 0; i < num.length;i++){
+//     sum += num[i];
+// }
+
+// console.log(sum); //6
+// bai 2
+// let num =[1,2,3]
+// let i = 0
+// for (let i = 0; i < array.length; i++) {
+    
+//    if (num[i]>max){
+//     max=num[i];
+//    }
+// }
+// console.log()
+// bai 3
+// let num =[1,2,3]
+// let i = 0
+// for (let i = 0; i < array.length; i++) {
+    
+//    if (num[i]>min){
+//     min=num[i];
+//    }
+// }
+// console.log()
+// bai 4
+// let value = +prompt('enter a number')
+// let num=[1, 2, 3, 4, 5]
+// for (let i = 0; i < array.length; i++) {
+//     if(value==num[i]){
+//         alert("Value " + numbers[i] + "found at " + i);
+//     }
+// }
+// bai 5
+// let num =[1, 2, 3, 4, 5]
+// let newnum = num.reverse()
+// console.log(num)
+// console.log(newnum)
+// bai 6
+// let num = [1,2,3,4,5]
+// let i = 0
+// for (let i = 0; i < array.length; i++) {
+//     if(
+//         num(i) % 2 == 0
+//     ){alert('la so chan')}
+// } console.log(`day la so chan ${num(i)}`)
+// bai 7
+// let arr = [1, 2, 3, 2, 4, 2, 5]
+// let count = 0
+// for (let i = 0; i < array.length; i++) {
+//     if(arr(i)=2)
+//     count++;
+// } 
+// console.log( "Phan tu " + 2  + " xuat hien " + count +  " lan");
+// bai 8
+// let num =[4, 2, 9, 5, 1]
+// score.sort(function(a,b){return b-a})
+// console.log=[1, 2, 4, 5, 9]
